5b8d53a6e9b4dc3813ab7a783a62010c3e8fa29d
diogosalvadorb <diogosalvadorb@gmail.com>
2025-11-28 16:17:22 -0300

    First Commit

---
+++ docker-working/docker-working/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace docker_working.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;  
+        public StatusController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            var environmnetDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+            return Ok(new
+            {
+                Mensagem = "Tudo funcionando corretamente!",
+                Ambiente = _env.EnvironmentName,
+                RodandoNoDocker = environmnetDocker,
+                HostName = Environment.MachineName,
+                Timestamp = DateTime.UtcNow,
+            }); 
+        }
+    }
+}
